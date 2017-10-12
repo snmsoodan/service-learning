a577e81fc9c3bb291321a3df7a609a5cad781bb7
sanamdeep singh <sanamsoodan@gmail.com>
2017-10-12 17:13:04 -0400

    first commit: basic layout

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// configure a public directory to host static content
+app.use(express.static(__dirname + '/public'));
+
+require ("./test/app.js")(app);
+
+// var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+// var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+//
+// app.listen(port, ipaddress);
+
+//console.log(process.env);
+
+// app.listen(3000);
+
+port = process.env.PORT || 3000;
+app.listen(port);
